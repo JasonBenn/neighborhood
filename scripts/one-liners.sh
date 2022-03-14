09308c80a5ce4034fb960365e6b2a4e3dd9895e7
Jason Benn <jasoncbenn@gmail.com>
2022-03-14 15:57:19 -0700

    Backup DB.

--- scripts/one-liners.sh
+++ scripts/one-liners.sh
@@ -13,3 +13,5 @@
 
 # Database
 pg_dump neighborhood --table houses_zillowdata -f /Users/jasonbenn/code/neighborhood/neighborhood/data/backups/2022-03-07_zillow.sql
+
+pg_dump neighborhood -f /Users/jasonbenn/Desktop/data/backups/2022-03-14_neighborhood_ratings.sql
