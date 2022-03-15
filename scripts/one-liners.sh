4fa6b5c3071e82b20ca4531985963032ae6b6b76
Jason Benn <jasoncbenn@gmail.com>
2022-03-15 02:16:09 -0700

    Helpful one-liners.

--- scripts/one-liners.sh
+++ scripts/one-liners.sh
@@ -2,6 +2,9 @@
 
 # Deploying
 ~/.pyenv/shims/python neighborhood/manage.py collectstatic  # push new styles to s3
+/Users/jasonbenn/.pyenv/versions/3.10.0/envs/neighborhood/bin/gunicorn --chdir neighborhood neighborhood.wsgi
+heroku config
+heroku logs --tail
 
 # Env
 ~/.pyenv/shims/pip install -r requirements.txt
@@ -14,4 +17,5 @@
 # Database
 pg_dump neighborhood --table houses_zillowdata -f /Users/jasonbenn/code/neighborhood/neighborhood/data/backups/2022-03-07_zillow.sql
 pg_dump neighborhood -f /Users/jasonbenn/Desktop/data/backups/2022-03-14_pm_neighborhood_ratings.sql
-/usr/local/Cellar/postgresql/14.2_1/bin/pg_restore pg_restore --verbose --clean --no-acl --no-owner -h localhost -U jasonbenn -d neighborhood latest.dump
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U jasonbenn -d neighborhood latest.dump
+# CAREFUL: heroku pg:push postgresql://localhost:5432/neighborhood DATABASE_URL
